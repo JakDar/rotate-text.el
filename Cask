abef64ece08e4ca0f259da58aaff6498239c4831
Jakub Darul <daruljakub@protonmail.com>
2021-04-20 20:50:09 +0200

    parens + CI + packaging
    also - add shell.nix and direnv

---
+++ Cask
@@ -0,0 +1,12 @@
+(source melpa)
+(source gnu)
+
+(package "rotate-text" "0.1"
+         "rotate-text allows you cycle through commonly interchanged text with a single keystroke.")
+
+(depends-on "cl-lib" "0.3")
+(depends-on "dash" "2.13.0")
+
+(development
+  (depends-on "smartparens")
+  )
---
+++ rotate-text-paren.el
@@ -0,0 +1,103 @@
+;;; rotate-text-paren.el --- Description -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2021
+;;
+;; Author:  <https://github.com/JakDar>
+;; Created: April 20, 2021
+;; Modified: April 20, 2021
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "27.1"))
+;; Homepage: https://github.com/JakDar/rotate-text.el
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;;  Description
+;;
+;;; Code:
+
+(eval-when-compile (require 'cl-lib))
+
+(defvar rotate-text-paren-pairs  '(("()" "{}" "[]"))
+  "*List of paren pair groups to rotate.")
+
+(defvar rotate-text-paren-get-paren-data-function nil
+  "Function to find paren position for current point.
+Return (list :start <pos-before-start-char> :end <pos-before-end-char> :left <left-paren-char>).
+Defaults to `rotate-text-paren-show-paren-get-data' if set to nil.")
+
+(defun rotate-text-paren--symbols ()
+  "."
+  (mapcar
+
+   (lambda (pairs)
+     (mapcan
+      (lambda (pair)
+        (list
+         (string-to-char (substring pair 0 1))
+         (string-to-char (substring pair 1 2))))
+      pairs)) rotate-text-paren-pairs))
+
+(defun rotate-text-paren--left->pair (paren-group) ;TODO: cleanup
+  "."
+  (let ((startchars  (mapcar (lambda (pair) (substring pair 0 1) ) paren-group ))
+        (rotated-pairs (cons (car (last paren-group)) (butlast paren-group))))
+    (cl-mapcar #'cons startchars rotated-pairs)))
+
+(defun rotate-text-paren-rotate-parens ()
+  "Toggle parens at cursor.
+Utilizes modified `rotate-text-paren-get-paren-data-function'
+with smartparens or show-paren backend to find and replace them
+with the other pair of brackets. This function can be easily
+modified and expanded to replace other brackets. Currently,
+mismatch information is ignored and mismatched parens are changed
+based on the left one."
+  (interactive)
+  (save-excursion
+    (let* ((parens (if rotate-text-paren-get-paren-data-function
+                       (funcall rotate-text-paren-get-paren-data-function)
+                     (progn
+                       (require 'rotate-text-paren-show-paren)
+                       (rotate-text-paren-show-paren-get-data))))
+           (start (plist-get parens :start))
+           (end (plist-get parens :end))
+           (startchar (plist-get parens :left)))
+      (when parens
+        (let
+            ((pair
+              (cl-find-if
+               (lambda (p) (string= (car p) startchar ))
+               (rotate-text-paren--left->pair (car rotate-text-paren-pairs)) ;; TODO - support mupltiple pairs
+               )))
+          (if pair
+              (rotate-text-paren--replace (cdr pair) start end)
+            (message "Missing paren. starchar: [%s] , parens: [%S]"
+                     startchar
+                     (rotate-text-paren--left->pair (car rotate-text-paren-pairs)))))))))
+
+(defun rotate-text-paren--replace (pair start end)
+  "Replace parens with a new PAIR at START and END in current buffer.
+
+A helper function for `rotate-text-paren-rotate-parens'."
+  (goto-char start)
+  (delete-char 1)
+  (insert (substring pair 0 1))
+  (goto-char end)
+  (delete-char 1)
+  (insert (substring pair 1 2)))
+
+
+(defun rotate-text-paren-or-text(arg &optional default-string com-symbols com-words com-patterns)
+  "."
+  (interactive (list (if (consp current-prefix-arg)
+                         -1
+                       (prefix-numeric-value current-prefix-arg))))
+  (if (cl-find (char-after) (flatten-list (rotate-text-paren--symbols)))
+      (rotate-text-paren-rotate-parens)
+    (rotate-text arg default-string com-symbols com-words com-patterns)))
+
+
+
+(provide 'rotate-text-paren)
+;;; rotate-text-paren.el ends here
